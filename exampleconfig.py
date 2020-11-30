61ec260b97271ac9ed0e184525dc56bb0ccb508a
SolB77 <52132274+SolB77@users.noreply.github.com>
2020-11-30 04:59:07 -0500

    add example config

---
+++ exampleconfig.py
@@ -0,0 +1,6 @@
+EMAIL_ADDRESS = 'youremail@gmail.com'
+PASSWORD = 'yourgmailpassword'
+STUSER = 'usernameforstocktrakplatform'
+STPASSWORD = 'passwordforstocktrackplatform'
+GROUP_EMAILS = ['list','of','group','emails']
+portfoliodatafolder = 'path/to/folder/with/portfolio/data'
